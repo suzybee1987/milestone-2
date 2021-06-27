a85ac03359fb6010157bb0c3e8fe48a0d53d888a
suzybee1987 <suzannemiller1987@gmail.com>
2021-06-27 10:47:08 +0000

    Dev: Create contact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_hmey31f", "ms2", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+};

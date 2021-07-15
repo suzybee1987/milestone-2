aeee2316c8021a5e935e52b274a81a1999dca2f6
suzybee1987 <suzannemiller1987@gmail.com>
2021-07-15 07:33:38 +0000

    Dev: Reorganise character vars, return answers to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,125 +1,71 @@
 // variables 
 const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-// user keys and values
-const death = [
-	"Death",
-	[
-		"The Colour of Magic", "The Light Fantastic", "Equal Rites", "Mort", "Sourcery", "Wyrd Sisters",
-		"Pyramids", "Guards! Guards!", "Eric", "Moving Pictures", "Reaper Man", "Witches Abroad", "Small Gods",
-		"Lords and Ladies", "Men At Arms", "Soul Music", "Interesting Times", "Maskerade", "Feet of Clay",
-		"Hogfather", "Jingo", "The Last Continent", "Carpe Jugulumn", "The Fifth Elephant", "The Truth", "Thief of Time",
-		"The Last Hero", "The Amazing Maurice and His Educated Rodents", "Night Watch", "Monstrous Regiment", "A Hat Full of Sky",
-		"Going Postal", "Thud!", "Wintersmith", "Making Money", "Unseen Academicals", "I Shall Wear Midnight",
-		"Raising Steam", "The Shepherd's Crown"
-	],
-	[{
-		1: "I COULD MURDER A CURRY",
-		3: "I'M HANDY WITH A SCYTHE",
-		4: "CATS. CATS ARE NICE",
-		5: "BINKY",
-		6: "A MYSTERY NOVEL",
-		8: "I DON'T EAT MY FRIENDS",
-		9: "NOBBY NOBBS"
-	}]
-];
+const characters = {
+	death: {
+		name: "Death",
+		books: [
+			"The Colour of Magic", "The Light Fantastic", "Equal Rites", "Mort", "Sourcery", "Wyrd Sisters",
+			"Pyramids", "Guards! Guards!", "Eric", "Moving Pictures", "Reaper Man", "Witches Abroad", "Small Gods",
+			"Lords and Ladies", "Men At Arms", "Soul Music", "Interesting Times", "Maskerade", "Feet of Clay",
+			"Hogfather", "Jingo", "The Last Continent", "Carpe Jugulumn", "The Fifth Elephant", "The Truth", "Thief of Time",
+			"The Last Hero", "The Amazing Maurice and His Educated Rodents", "Night Watch", "Monstrous Regiment", "A Hat Full of Sky",
+			"Going Postal", "Thud!", "Wintersmith", "Making Money", "Unseen Academicals", "I Shall Wear Midnight",
+			"Raising Steam", "The Shepherd's Crown"
+		],
 
-const nanny = [
-	"Nanny Ogg",
-	[
-		"Wyrd Sisters", "Witches Abroad", "Lords and Ladies", "Maskerade", "Carpe Jugulum",
-		"The Sea and Little Fishes", "Wintersmith", "I Shall Wear Midnight", "The Shepherd's Crown"
-	],
-	[{
-		4: "My handsome Greebo",
-		5: "Broomstick",
-		6: "My Cookbook",
-		7: "Scumble and a nice man ;)",
-		10: "Don't do anything I wouldn't do, if you ever find anything I wouldn't do."
-	}]
-];
+	},
 
-const librarian = [
-	"The Librarian",
-	[
-		"The Light Fantastic", "Equal Rites", "Sourcery", "Guards! Guards!", "Eric", "Reaper Man", "Men at Arms",
-		"Soul Music", "Interesting Times", "Maskerade", "The Last Continent", "The Last Hero", "The Science of Discworld",
-		"The Science of Discworld II: the Globe", "The Science of Discworld III: Darwin 's Watch", "Unseen Academicals"
-	],
-	[{
-		2: "Going to the pub and eating peanuts",
-		3: "Attacking anyone who calls me a monkey",
-		7: "Bananas and Organ Music",
-		10: "OOK!"
-	}]
-];
+	nanny: {
+		name: "Nanny Ogg",
+		books: [
+			"Wyrd Sisters", "Witches Abroad", "Lords and Ladies", "Maskerade", "Carpe Jugulum",
+			"The Sea and Little Fishes", "Wintersmith", "I Shall Wear Midnight", "The Shepherd's Crown"
+		],
+	},
 
-const vetinari = [
-	"Lord Vetinari",
-	[
-		"Sourcery", "Guards! Guards!", "Moving Pictures", "Men at Arms", "Interesting Times",
-		"Feet of Clay", "The Fifth Elephant", "The Truth", "Going Postal", "Thud!", "Making Money",
-		"Unseen Academicals", "Raising Steam", "The Colour of Magic", "Soul Music", "The Last Hero",
-		"Night Watch"
-	],
-	[{
-		2: "Playing a board game with a friend",
-		4: "Dogs, of course",
-		5: "Coach",
-		6: "Written music rather than listening to it performed",
-		8: "I'd rather train them",
-		9: "Cecil Wormsborough St. John 'Nobby' Nobbs"
-	}]
-];
+	librarian: {
+		name: "The Librarian",
+		books: [
+			"The Light Fantastic", "Equal Rites", "Sourcery", "Guards! Guards!", "Eric", "Reaper Man", "Men at Arms",
+			"Soul Music", "Interesting Times", "Maskerade", "The Last Continent", "The Last Hero", "The Science of Discworld",
+			"The Science of Discworld II: the Globe", "The Science of Discworld III: Darwin 's Watch", "Unseen Academicals"
+		],
+	},
 
-const rob = [
-	"Rob Anybody",
-	[
-		"A Hat Full of Sky", "I Shall Wear Midnight", "The Shepherd's Crown", "The Wee Free Men", "Wintersmith"
-	],
-	[{
-		1: "Food? I'd rather drink! Anything vaguely alcoholic.",
-		2: "Drinking, fighting and stealing",
-		3: "Drinking, fighting and stealing",
-		4: "Ships!",
-		9: "Daft Wullie",
-		10: "CRIVENS!"
-	}]
-];
+	vetinari: {
+		name: "Lord Vetinari",
+		books: [
+			"Sourcery", "Guards! Guards!", "Moving Pictures", "Men at Arms", "Interesting Times",
+			"Feet of Clay", "The Fifth Elephant", "The Truth", "Going Postal", "Thud!", "Making Money",
+			"Unseen Academicals", "Raising Steam", "The Colour of Magic", "Soul Music", "The Last Hero",
+			"Night Watch"
+		],
+	},
 
-const gaspode = [
-	"Gaspode",
-	[
-		"Moving Pictures", "Men at Arms", "The Fifth Elephant", "The Truth", "Soul Music", "Hogfather", "Feet of Clay"
-	],
-	[{
-		1: "Pride is all very well, but a sausage is a sausage",
-		7: "A warm place by the fire and NO BATHS",
-		8: "Abso-woof-ly",
-		10: "Bark"
-	}]
-];
+	rob: {
+		name: "Rob Anybody",
+		books: [
+			"A Hat Full of Sky", "I Shall Wear Midnight", "The Shepherd's Crown", "The Wee Free Men", "Wintersmith"
+		],
+	},
 
-const vimes = [
-	"Commander Vimes",
-	[
-		"Guards! Guards!", "Men At Arms", "Feet of Clay", "Jingo", "The Fifth Elephant",
-		"The Truth", "Night Watch", "Monstrous Regiment", "Thud!", "Where's My Cow?", "Making Money",
-		"Unseen Academicals", "Snuff", "Raising Steam"
-	],
-	[{
-		1: "A BLT, heavy on the B",
-		3: "Chasing criminals and setting traps for assassins",
-		5: "My own feet",
-		6: "Where's My Cow?",
-		7: "A good cigar",
-		8: "Only if I was really hungry",
-		9: "Nobby Nobbs"
-	}]
-];
-
+	gaspode: {
+		name: "Gaspode",
+		books: [
+			"Moving Pictures", "Men at Arms", "The Fifth Elephant", "The Truth", "Soul Music", "Hogfather", "Feet of Clay"
+		],
+	},
+	vimes: {
+		name: "Commander Vimes",
+		books: [
+			"Guards! Guards!", "Men At Arms", "Feet of Clay", "Jingo", "The Fifth Elephant",
+			"The Truth", "Night Watch", "Monstrous Regiment", "Thud!", "Where's My Cow?", "Making Money",
+			"Unseen Academicals", "Snuff", "Raising Steam"
+		],
+	}
+};
 
 // define questionList 
 let questionList = null;
@@ -146,10 +92,10 @@ const getJsonThenLoad = function () {
 
 
 function buildQuiz() {
-	//  let answerButton = document.getElementsByClassName('answer');
+	// start the game 
 	startGame();
 
-	// function to start the game 
+	// function to start the game and load questions
 	function startGame() {
 
 		// gameOutput is an array 
@@ -158,11 +104,11 @@ function buildQuiz() {
 		// for each question get current question and number 
 		questionList.forEach(
 			(currentQ, qNumber) => {
+
 				// variable to store answers 
-
 				const answers = [];
-				// for each answer add a button to click 
 
+				// for each answer add a button to click 
 				for (option in currentQ.answers) {
 					answers.push(
 						`<div class="col-sm-6 m-auto">
@@ -187,7 +133,7 @@ function buildQuiz() {
 		quizContainer.innerHTML = gameOutput.join('');
 	};
 
-	function getResults() {
+	function getResults(value) {
 
 		// container to put the answers in to 
 		const answerContainers = quizContainer.querySelectorAll('.answers');
@@ -195,76 +141,93 @@ function buildQuiz() {
 		// so that each game starts with zero questions asked 
 		let questionsAsked = 0;
 
+		// keys in here
 		let userAnswers = [];
+		
+		// each character appears the number of times selected
+		// use filter will give array and count length
 
 		questionList.forEach((questionId, qNumber) => {
 
-				//find selected answer
-				const answerContainer = answerContainers[qNumber];
-				const selector = `input[name=question${qNumber}]:checked`;
-				let userAnswer = (answerContainer.querySelector(selector) || {}).value;
+			//find selected answer
+			const answerContainer = answerContainers[qNumber];
+			const selector = `input[value=${option}]:checked`;
+			let userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-				questionsAsked++;
+			userAnswers.push(value);
+			console.log(userAnswers);
+
+			questionsAsked++;
+
 		});
-}
-
-// find function
-
-function checkAnswer(userAnswer) {
-
-	// this is code from the hackathon and explained by Tobi. find() needs to return a callback function 
-	// answer: function (questionId, givenAnswer) {
-	// 	givenAnswers += 1;
-	// 	questionsAsked.push(questionId);
-
-	// 	const question = questionList.find(d => d.id === questionId);
-
-	// 	if (givenAnswer === question.correct) {
-	// 		correctAnswers += 1;
-	// 	}
-
-	// 	return question.correct;
-	// },
-
-	// use includes?
-	// if (questionList.includes(userAnswer)) {
-
-	// 	answerContainer.push(userAnswer);
+		// this is coming back undefined 
+		// userAnswers.forEach(character => console.log(character));
 
 
-};
-
-// pagination when answer button clicked 
-
-function showSlide(n) {
-	slides[currentSlide].classList.remove('active-slide');
-	slides[n].classList.add('active-slide');
-	currentSlide = n;
-	if (currentSlide === slides.length + 1) {
-		slides.classList.remove('active-slide');
-		getResults();
 	}
-}
 
-// pagination show next question 
-function showNextSlide() {
-	showSlide(currentSlide + 1);
-}
+	// find function
 
-function showPreviousSlide() {
-	showSlide(currentSlide - 1);
-}
+	function checkAnswer(userAnswer) {
 
-let slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
+		// this is code from the hackathon and explained by Tobi. find() needs to return a callback function 
+		// answer: function (questionId, givenAnswer) {
+		// 	givenAnswers += 1;
+		// 	questionsAsked.push(questionId);
 
-showSlide(currentSlide);
+		// 	const question = questionList.find(d => d.id === questionId);
+
+		// 	if (givenAnswer === question.correct) {
+		// 		correctAnswers += 1;
+		// 	}
+
+		// 	return question.correct;
+		// },
+
+		// // use includes?
+		// if (questionList.includes(userAnswer)) {
+
+		// 	answerContainer.push(userAnswer);
 
 
-// jquery listening for answer clicked and displaying next question
+	};
 
-$('.answer').click(function () {
+	// pagination when answer button clicked 
 
-	showNextSlide();
-});
+	function showSlide(n) {
+		slides[currentSlide].classList.remove('active-slide');
+		slides[n].classList.add('active-slide');
+		currentSlide = n;
+		if (currentSlide === slides.length - 1) {
+			slides.classList.remove('active-slide');
+			submitButton.style.display = 'inline-block';
+		} else {
+			submitButton.style.display = 'none';
+		}
+	}
+
+	// pagination show next question 
+	function showNextSlide() {
+		showSlide(currentSlide + 1);
+	}
+
+	function showPreviousSlide() {
+		showSlide(currentSlide - 1);
+	}
+
+	let slides = document.querySelectorAll(".slide");
+	let currentSlide = 0;
+
+	showSlide(currentSlide);
+
+
+	// jquery listening for answer clicked and displaying next question
+
+	$('.answer').click(function () {
+		if (currentSlide < slides.length - 1) {
+			showNextSlide();
+		} else {
+			getResults();
+		}
+	});
 };
